d0a62c06045306fd7de1fb51378b4d0254c203de
Laurent Jourdren <jourdren@biologie.ens.fr>
2008-11-12 10:20:25 +0000

    Select algorithm now use window step parameter

--- src/main/java/fr/ens/transcriptome/oligo/Select.java
+++ src/main/java/fr/ens/transcriptome/oligo/Select.java
@@ -49,7 +49,7 @@ public class Select {
     /**
      * Define the weigths to use for selection
      * @param sm SequenceMeasurement that contains all the measurements to use
-     *            by the selection algorithm.
+     *          by the selection algorithm.
      */
     public abstract void setWeights(final SequenceMeasurements sm);
   };
@@ -121,13 +121,14 @@ public class Select {
    * @param statsFile stats file for the measurements input file
    * @param outputFile output file to generate with the selected oligos
    * @param weightsSetters Weight to apply on measurements
-   * @param windowSize size of the window
+   * @param windowLength size of the window
+   * @param windowStep step for the windows
    * @throws IOException if an error occurs while reading/writing input/output
-   *             files
+   *           files
    */
   public static void select(final File inputFile, final File statsFile,
-      File outputFile, final WeightsSetter weightsSetters, final int windowSize)
-      throws IOException {
+      File outputFile, final WeightsSetter weightsSetters,
+      final int windowLength, final int windowStep) throws IOException {
 
     // Open measurement file
     final SequenceMeasurementsReader smr =
@@ -147,12 +148,18 @@ public class Select {
     int infoCountWindows = 1;
     int infoCountSelectedOligos = 0;
 
-    int max = windowSize;
+    int endWindow = windowLength;
+    int startWindow = 0;
 
     float bestScore = MIN_SCORE;
+    float nextBestScore = MIN_SCORE;
+    int posNextBestScore = -1;
+    int lastSelected = -1;
 
     // Create Object used to write selected oligo
     final SequenceMeasurements smToWrite = new SequenceMeasurements();
+
+    final SequenceMeasurements nextSmToWrite = new SequenceMeasurements();
 
     // Open output file
     final SequenceMeasurementsWriter smw =
@@ -168,11 +175,14 @@ public class Select {
         valuesLength = values.length;
 
         // Add measurement field in output file
-        for (Measurement m : sm.getMeasurements())
+        for (Measurement m : sm.getMeasurements()) {
           smToWrite.addMesurement(m);
+          nextSmToWrite.addMesurement(m);
+        }
 
         // Add Global score to the output file
         smToWrite.addMesurement(new GlobalScoreMeasurement());
+        nextSmToWrite.addMesurement(new GlobalScoreMeasurement());
 
         // Read stats
         SequenceMeasurementsStatReader smsr =
@@ -207,50 +217,83 @@ public class Select {
 
         // Write best
         if (bestScore > MIN_SCORE) {
-          smw.writeSequenceMesurement(smToWrite);
 
-          infoCountSelectedOligos++;
+          // Prevent writing the same oligo more than one time
+          if (lastSelected != smToWrite.getId()) {
+
+            smw.writeSequenceMesurement(smToWrite);
+            lastSelected = smToWrite.getId();
+            infoCountSelectedOligos++;
+          }
         } else
           logger.severe("Bad case while selecting (1): " + bestScore);
 
-        logger.fine(String.format("scaffold: %s\t%d windows (%.2f theoric), "
-            + "%d oligos selected, %d pb in scaffold, %d pb windows.",
-            currentScafold, infoCountWindows,
-            (float) infoLastIndexStartPosition / (float) windowSize,
-            infoCountSelectedOligos, infoLastIndexStartPosition, windowSize));
+        logger
+            .fine(String
+                .format(
+                    "scaffold: %s\t%d windows (%.2f theoric), "
+                        + "%d oligos selected, %d pb in scaffold, %d pb windows, %d pb step.",
+                    currentScafold,
+                    infoCountWindows,
+                    ((infoLastIndexStartPosition + 1.0f - windowLength) / windowStep) + 1.0f,
+                    infoCountSelectedOligos, infoLastIndexStartPosition,
+                    windowLength, windowStep));
 
         infoCountSelectedOligos = 0;
 
         currentScafold = scaffold;
-        max = windowSize;
+        endWindow = windowLength;
+        startWindow = 0;
         infoCountWindows = 1;
 
-        while (pos >= max) {
-          max += windowSize;
+        while (pos >= endWindow) {
+          startWindow = endWindow + 1;
+          endWindow += windowStep;
           infoCountWindows++;
         }
 
         bestScore = MIN_SCORE;
+        nextBestScore = MIN_SCORE;
+        posNextBestScore = -1;
       }
 
-      if (pos >= max) {
+      if (pos >= endWindow) {
         // Write best
         if (bestScore > MIN_SCORE) {
 
-          smw.writeSequenceMesurement(smToWrite);
-
-          infoCountSelectedOligos++;
+          // Prevent writing the same oligo more than one time
+          if (lastSelected != smToWrite.getId()) {
+            smw.writeSequenceMesurement(smToWrite);
+            lastSelected = smToWrite.getId();
+            infoCountSelectedOligos++;
+          }
         } else
           logger.severe("Bad case while selecting (2): " + bestScore);
 
         bestScore = MIN_SCORE;
-        while (pos >= max) {
-          max += windowSize;
+
+        final int previousEndWindow = endWindow;
+
+        while (pos >= endWindow) {
+          startWindow = endWindow + 1;
+          endWindow += windowStep;
           infoCountWindows++;
         }
+
+        // Test if the best score is also the best score for next window
+        if (endWindow - windowStep == previousEndWindow) {
+
+          bestScore = nextBestScore;
+
+          // Test if the best score is also the best score for next next window
+          if (posNextBestScore < startWindow + windowStep)
+            nextBestScore = MIN_SCORE;
+        }
+
       }
 
       final float score = sm.getScore();
+      boolean bestScoreChanged = false;
 
       if (score > bestScore) {
 
@@ -263,6 +306,28 @@ public class Select {
         valuesToWrite[valuesLength] = score;
 
         smToWrite.setArrayMeasurementValues(valuesToWrite);
+        bestScoreChanged = true;
+      }
+
+      if ((pos >= startWindow + windowStep) && score > nextBestScore) {
+
+        nextBestScore = score;
+        posNextBestScore = pos;
+        nextSmToWrite.setId(id);
+
+        if (bestScoreChanged) {
+
+          nextSmToWrite.setArrayMeasurementValues(smToWrite
+              .getArrayMeasurementValues());
+        } else {
+
+          // Add the global score
+          final Object[] valuesToWrite = new Object[valuesLength + 1];
+          System.arraycopy(values, 0, valuesToWrite, 0, valuesLength);
+          valuesToWrite[valuesLength] = score;
+
+          nextSmToWrite.setArrayMeasurementValues(valuesToWrite);
+        }
       }
 
       infoLastIndexStartPosition = pos;
@@ -271,18 +336,20 @@ public class Select {
     // Write best
     if (bestScore > MIN_SCORE) {
 
-      logger.fine("*** select "
-          + smToWrite.getId() + " for window " + infoCountWindows
-          + "\tbestScore=" + bestScore + " ***");
+      // logger.fine("*** select "
+      // + smToWrite.getId() + " for window " + infoCountWindows
+      // + "\tbestScore=" + bestScore + " ***");
 
-      smw.writeSequenceMesurement(smToWrite);
+      // Prevent writing the same oligo more than one time
+      if (lastSelected != smToWrite.getId())
+        smw.writeSequenceMesurement(smToWrite);
     }
 
     logger.fine(String.format("scaffold: %s\t%d windows (%.2f theoric), "
-        + "%d oligos selected, %d pb in scaffold, %d pb windows.",
+        + "%d oligos selected, %d pb in scaffold, %d pb windows, %d pb step.",
         currentScafold, infoCountWindows, (float) infoLastIndexStartPosition
-            / (float) windowSize, infoCountSelectedOligos,
-        infoLastIndexStartPosition, windowSize));
+            / (float) windowLength, infoCountSelectedOligos,
+        infoLastIndexStartPosition, windowLength, windowStep));
 
     smw.close();
 
