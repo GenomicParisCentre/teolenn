652ae19326608f2cf84cd0b7e20b3e8097909343
Laurent Jourdren <jourdren@biologie.ens.fr>
2008-11-21 17:01:55 +0000

    Update teolenn.sh

--- src/main/bin/teolenn.sh
+++ src/main/bin/teolenn.sh
@@ -1,9 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # This script set the right classpath and start the application
 #
 
+# Get the path to this script
 BASEDIR=`dirname $0`
+
+# Set the Toelenn libraries path
 LIBDIR=$BASEDIR/lib
-java -server -Xmx512m -cp $LIBDIR/teolenn.jar:$LIBDIR/commons-math.jar:$LIBDIR/dom4j.jar:$LIBDIR/commons-cli.jar fr.ens.transcriptome.teolenn.Main $*
+
+# Set the memory in MiB needed by Teolenn (only Java part, not external tools)
+# By Default 512
+MEMORY=512
+
+# Add here your plugins and dependencies
+PLUGINS=
+
+# Set the path to java
+JAVA_CMD=java
+
+# Launch Toelenn
+$JAVA_CMD -server \
+          -Xmx${MEMORY}m \
+          -cp $LIBDIR/teolenn.jar:$LIBDIR/commons-math.jar:$LIBDIR/dom4j.jar:$LIBDIR/commons-cli.jar:$PLUGINS \
+          fr.ens.transcriptome.teolenn.Main $*
