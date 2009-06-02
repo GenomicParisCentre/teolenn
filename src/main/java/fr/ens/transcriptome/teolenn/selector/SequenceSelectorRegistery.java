b3e1199540ef393311810141d9299f41d60d8208
Laurent Jourdren <jourdren@biologie.ens.fr>
2009-06-02 12:38:00 +0000

    Sequence Selector inherits from Module.

--- src/main/java/fr/ens/transcriptome/teolenn/selector/SequenceSelectorRegistery.java
+++ src/main/java/fr/ens/transcriptome/teolenn/selector/SequenceSelectorRegistery.java
@@ -137,7 +137,7 @@ public class SequenceSelectorRegistery {
 
   static {
 
-    addSequenceSelectorType(, TilingSelector.class);
+    addSequenceSelectorType(TilingSelector.SELECTOR_NAME, TilingSelector.class);
   }
 
 }
